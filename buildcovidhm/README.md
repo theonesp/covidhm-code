921518539b4089be1ff38ef950eb0443ef3adfe6
theonesp <ma.armengol.hcsc@gmail.com>
2020-07-09 00:10:16 -0400

    covid dsl v2 descriptive manuescript

--- buildcovidhm/README.md
+++ buildcovidhm/README.md
@@ -1,2 +1,3 @@
 # Building the covidhm database
 This directory contains scripts that can be used to create a new instance of the covid Database.
+*Please carefully review the warnings inside the '03_postgres_create_tables.sql' file when importing the data.*
--- buildcovidhm/postgres/03_postgres_create_tables.sql
+++ buildcovidhm/postgres/03_postgres_create_tables.sql
@@ -5,72 +5,77 @@
 -- -------------------------------------------------------------------------------
 
 --------------------------------------------------------
---  File created - 04/29/2020
+--  File created - 07/04/2020
 --------------------------------------------------------
 
 -- If running scripts individually, you can set the schema where all tables are created as follows:
--- SET search_path TO covidhm_24_04_2020; -- or your schema name
+-- SET search_path TO covid_dsl_v2; -- or your schema name
 
 --------------------------------------------------------
 --  DDL for Table patient
 --------------------------------------------------------
-
+-- WARNING: When importing this file use:
+-- utf8 encoding.
+-- replace ',' with '.' in temperature
 
 -- Drop table
 
 -- DROP TABLE patient;
 
 CREATE TABLE patient (
-	patient_id varchar NULL,
-	edad_age varchar NULL,
-	sexo_sex varchar NULL,
-	diag_ing_inpat varchar NULL,
-	f_ingreso_admission_d_ing_inpat varchar NULL,
-	f_entrada_uc_icu_date_in varchar NULL,
-	f_salida_uci_icu_date_out varchar NULL,
-	uci_dias_icu_days varchar NULL,
-	f_alta_discharge_date_ing varchar NULL,
-	motivo_alta_destiny_discharge_ing varchar NULL,
-	f_ingreso_admission_date_urg_emerg varchar NULL,
-	hora_time_admision_admission_urg_emerg varchar NULL,
-	especialidad_department_urg_emerg varchar NULL,
-	diag_urg_emerg varchar NULL,
-	destino_destiny_urg_emerg varchar NULL,
-	hora_time_constant_primera_first_urg_emerg varchar NULL,
-	temp_primera_first_urg_emerg varchar NULL,
-	fc_hr_primera_first_urg_emerg varchar NULL,
-	glu_primera_first_urg_emerg varchar NULL,
-	sat_02_primera_first_urg_emerg varchar NULL,
-	ta_max_primera_first_emerg_urg varchar NULL,
-	ta_min_primera_first_urg_emerg varchar NULL,
-	hora_time_constant_ultima_last_urg_emerg varchar NULL,
-	fc_hr_ultima_last_urg_emerg varchar NULL,
-	temp_ultima_last_urg_emerg varchar NULL,
-	glu_ultima_last_urg_emerg varchar NULL,
-	sat_02_ultima_last_urg_emerg varchar NULL,
-	ta_max_ultima_last_urgemerg varchar NULL,
-	ta_min_ultima_last_urg_emerg varchar NULL
+	patient_id int4 NULL,
+	age int4 NULL,
+	sex varchar NULL,
+	diag_inpat varchar NULL,
+	admission_d_inpat timestamp NULL,
+	icu_date_in timestamp NULL,
+	icu_date_out timestamp NULL,
+	icu_days int4 NULL,
+	mechvent varchar NULL,
+	discharge_date timestamp NULL,
+	destin_discharge varchar NULL,
+	admission_date_emerg timestamp NULL,
+	time_admission_emerg timestamp NULL,
+	department_emerg varchar NULL,
+	diag_emerg varchar NULL,
+	destin_emerg varchar NULL,
+	time_constant_first_emerg varchar NULL,
+	temp_first_emerg numeric NULL,
+	hr_first_emerg int4 NULL,
+	glu_first_emerg numeric NULL,
+	sat_02_first_emerg numeric NULL,
+	bp_max_first_emerg numeric NULL,
+	bp_min_first_emerg numeric NULL,
+	time_constant_last_emerg timestamp NULL,
+	hr_last_emerg numeric NULL,
+	temp_last_emerg numeric NULL,
+	glu_last_emerg numeric NULL,
+	sat_02_last_emerg numeric NULL,
+	bp_max_last_emerg numeric NULL,
+	bp_min_last_emerg numeric NULL
 );
+
 
 --------------------------------------------------------
 --  DDL for Table medication
 --------------------------------------------------------
-
+-- WARNING: When importing this file use:
+-- LATIN1 encoding, utf8 introduces problems when addressing the accents.
 
 -- Drop table
 
 -- DROP TABLE medication;
 
 CREATE TABLE medication (
-	patient_id varchar NULL,
-	farmaco_drug_nombre_comercial_comercial_name varchar NULL,
-	dosis_media_diaria_daily_avrg_dose varchar NULL,
-	inicio_trat_drug_start_date varchar NULL,
-	fin_trat_drug_end_date varchar NULL,
-	atc5_nombre_name varchar NULL,
+	patient_id int4 NULL,
+	drug_comercial_name varchar NULL,
 	id_atc5 varchar NULL,
-	atc7_nombre_name varchar NULL,
-	id_atc7 varchar NULL
+	atc5_name varchar NULL,
+	id_atc7 varchar NULL,
+	atc7_name varchar NULL,
+	daily_avrg_dose int4 NULL,
+	drug_start_date date NULL,
+	drug_end_date date NULL
 );
 
 --------------------------------------------------------
@@ -81,38 +86,42 @@ CREATE TABLE medication (
 -- Drop table
 
 -- DROP TABLE vital_sign;
+-- WARNING: In order to properly ingest the decimal data into postgres
+-- replace ',' with '.' in temp_ing column.
 
-CREATE TABLE vital_sign (
-	patient_id varchar NULL,
-	constants_ing_inpat_fecha_date varchar NULL,
-	constants_ing_inpat_hora_time varchar NULL,
-	fc_hr_ing_inpat varchar NULL,
-	glu_gly_ing_inpat varchar NULL,
-	sat_02_ing_inpat varchar NULL,
-	ta_max_ing_inpat varchar NULL,
-	ta_min_ing_inpat varchar NULL,
-	temp_ing_inpat varchar NULL
+CREATE TABLE vital_signs (
+	patient_id int4 NULL,
+	constants_ing_date date NULL,
+	constants_ing_time time NULL,
+	bp_max_ing numeric NULL,
+	bp_min_ing numeric NULL,
+	temp_ing float8 NULL,
+	hr_ing numeric NULL,
+	sat_02_ing numeric NULL,
+	sat_02_ing_obs numeric NULL,
+	glu_ing numeric NULL
 );
 
 
 --------------------------------------------------------
 --  DDL for Table laboratory
 --------------------------------------------------------
-
+-- WARNING: When importing this file use:
+-- LATIN1 encoding, utf8 introduces problems when addressing the accents.
 
 -- Drop table
 
 -- DROP TABLE laboratory;
 
 CREATE TABLE laboratory (
-	patient_id varchar NULL,
-	peticion_laboratorio_lab_number varchar NULL,
-	fecha_peticion_lab_date varchar NULL,
-	hora_peticion_time_lab varchar NULL,
-	determinacion_item_lab varchar NULL,
-	resultado_val_result varchar NULL,
-	unidades_ud_result varchar NULL,
-	valores_referencia_ref_values varchar NULL
+	patient_id int4 NULL,
+	lab_number varchar NULL,
+	lab_date date NULL,
+	time_lab time NOT NULL,
+	item_lab varchar NULL,
+	val_result varchar NULL,
+	ud_result varchar NULL,
+	ref_values varchar NULL
 );
 
 
@@ -126,7 +135,7 @@ CREATE TABLE laboratory (
 -- DROP TABLE diagnosis_er_adm;
 
 CREATE TABLE diagnosis_er_adm (
-	patient_id varchar NULL,
+    patient_id int4 NULL,
 	dia_ppal varchar NULL,
 	dia_02 varchar NULL,
 	dia_03 varchar NULL,
@@ -157,51 +166,45 @@ CREATE TABLE diagnosis_er_adm (
 -- DROP TABLE diagnosis_hosp_adm;
 
 CREATE TABLE diagnosis_hosp_adm (
-	patient_id varchar NULL,
+	patient_id int4 NULL,
 	dia_ppal varchar NULL,
-	dia_02 varchar NULL,
-	dia_03 varchar NULL,
-	dia_04 varchar NULL,
-	dia_05 varchar NULL,
-	dia_06 varchar NULL,
-	dia_08 varchar NULL,
-	dia_07 varchar NULL,
-	dia_09 varchar NULL,
-	dia_10 varchar NULL,
-	dia_11 varchar NULL,
-	dia_12 varchar NULL,
-	dia_13 varchar NULL,
-	dia_14 varchar NULL,
-	dia_15 varchar NULL,
-	dia_16 varchar NULL,
-	dia_17 varchar NULL,
-	dia_18 varchar NULL,
-	dia_19 varchar NULL,
-	neo_01 varchar NULL,
-	neo_02 varchar NULL,
-	neo_03 varchar NULL,
-	neo_04 varchar NULL,
-	neo_05 varchar NULL,
-	neo_06 varchar NULL,
-	poad_02 varchar NULL,
-	poad_03 varchar NULL,
-	poad_04 varchar NULL,
-	poad_05 varchar NULL,
-	poad_06 varchar NULL,
-	poad_07 varchar NULL,
-	poad_08 varchar NULL,
-	poad_09 varchar NULL,
-	poad_10 varchar NULL,
-	poad_11 varchar NULL,
-	poad_12 varchar NULL,
-	poad_13 varchar NULL,
-	poad_14 varchar NULL,
-	poad_15 varchar NULL,
-	poad_16 varchar NULL,
-	poad_17 varchar NULL,
-	poad_18 varchar NULL,
-	poad_19 varchar NULL,
 	poad_ppal varchar NULL,
+	dia_02 varchar NULL,
+	poad_02 varchar NULL,
+	dia_03 varchar NULL,
+	poad_03 varchar NULL,
+	dia_04 varchar NULL,
+	poad_04 varchar NULL,
+	dia_05 varchar NULL,
+	poad_05 varchar NULL,
+	dia_06 varchar NULL,
+	poad_06 varchar NULL,
+	dia_07 varchar NULL,
+	poad_07 varchar NULL,
+	dia_08 varchar NULL,
+	poad_08 varchar NULL,
+	dia_09 varchar NULL,
+	poad_09 varchar NULL,
+	dia_10 varchar NULL,
+	poad_10 varchar NULL,
+	dia_11 varchar NULL,
+	poad_11 varchar NULL,
+	dia_12 varchar NULL,
+	poad_12 varchar NULL,
+	dia_13 varchar NULL,
+	poad_13 varchar NULL,
+	dia_14 varchar NULL,
+	poad_14 varchar NULL,
+	dia_15 varchar NULL,
+	poad_15 varchar NULL,
+	dia_16 varchar NULL,
+	poad_16 varchar NULL,
+	dia_17 varchar NULL,
+	poad_17 varchar NULL,
+	dia_18 varchar NULL,
+	poad_18 varchar NULL,
+	dia_19 varchar NULL,
+	poad_19 varchar NULL,
 	proc_01 varchar NULL,
 	proc_02 varchar NULL,
 	proc_03 varchar NULL,
@@ -221,5 +224,31 @@ CREATE TABLE diagnosis_hosp_adm (
 	proc_17 varchar NULL,
 	proc_18 varchar NULL,
 	proc_19 varchar NULL,
-	proc_20 varchar NULL
+	proc_20 varchar NULL,
+	neo_01 varchar NULL,
+	neo_02 varchar NULL,
+	neo_03 varchar NULL,
+	neo_04 varchar NULL,
+	neo_05 varchar NULL,
+	neo_06 varchar NULL
+);
+
+
+--------------------------------------------------------
+--  DDL for Table icd10_codes_dict
+--------------------------------------------------------
+
+
+-- Drop table
+
+-- DROP TABLE icd10_codes_dict;
+-- This table was obtained from (kamillamagna/ICD-10-CSV, 2020)
+
+CREATE TABLE icd10_codes_dict(
+   cat_code VARCHAR
+  ,dx_code  INTEGER
+  ,code     VARCHAR
+  ,abbrev   VARCHAR
+  ,full     VARCHAR
+  ,title    VARCHAR
 );
